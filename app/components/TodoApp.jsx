481116770aefbd29cdeb1cb36d5219e1f0528e6f
Mark Sauter <Mark Sauter>
2016-10-14 10:08:23 -0500

    Initial commit

---
+++ app/components/TodoApp.jsx
@@ -0,0 +1,16 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+class TodoApp extends React.Component {
+
+  render () {
+    return (
+      <div>
+        <p>TodoApp.jsx</p>
+      </div>
+    );
+  }
+
+}
+
+module.exports = TodoApp;
