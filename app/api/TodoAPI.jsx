84d14446f40c9620b380a8d564528e7cdc9424d5
Mark Sauter <Mark Sauter>
2016-10-16 20:15:07 -0500

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,31 @@ export default {
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    // Filter by showCompleted
+    filteredTodos = filteredTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+    // Filter by searchText
+    filteredTodos = filteredTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return text.includes(searchText);
+    });
+    // Sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1;
+      }
+      if (a.completed && !b.completed) {
+        return 1;
+      }
+
+      return 0;
+    });
+
+    return filteredTodos;
   }
 };
