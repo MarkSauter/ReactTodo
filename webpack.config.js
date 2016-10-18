d94aa518b09c8cdcc3c164aea42338888631e7d5
Mark Sauter <Mark Sauter>
2016-10-18 12:13:17 -0500

    Add actions & action tests

--- webpack.config.js
+++ webpack.config.js
@@ -29,6 +29,7 @@ module.exports = {
     ],
     alias: {
       appplicationStyle: 'app/styles/app.scss',
+      actions: 'app/actions/actions.jsx'
     },
     extensions: ['', '.js', '.jsx']
   },
