8a7dadde5de7cfdf60ad3774b16ceafd88dfc90a
Mark Sauter <Mark Sauter>
2016-10-21 20:27:47 -0500

    Fixing heroku jquery bug...removed definePlugin from webpack config

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -1,4 +1,4 @@
-import $ from 'jquery'
+import $ from 'jQuery'
 
 export default {
   filterTodos: (todos, showCompleted, searchText) => {
--- webpack.config.js
+++ webpack.config.js
@@ -9,22 +9,19 @@ module.exports = {
     'script!foundation-sites/dist/foundation.min.js',
     './app/app.jsx'
   ],
-  // externals: {
-  //   jquery: 'jQuery'
-  // },
+  externals: {
+    jquery: 'jQuery'
+  },
   plugins: [
-    // new webpack.ProvidePlugin({
-    //   '$': 'jquery',
-    //   'jQuery': 'jquery',
-    //   'window.jQuery': 'jquery'
-    // }),
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery',
+      'window.jQuery': 'jquery'
+    }),
     new webpack.optimize.UglifyJsPlugin({
       compressor: {
         warnings: false
       }
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
     })
   ],
   output: {
